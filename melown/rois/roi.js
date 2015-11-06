423c66b033211dd31f70a1942149a4ff0c700874
Tomáš Kavan <tomas.kavan@citationtech.net>
2015-11-06 17:41:40 +0100

    Changed way to create a ROI instance (Fetch method).

--- melown/rois/roi.js
+++ melown/rois/roi.js
@@ -1,5 +1,7 @@
 /**
- * A ROI instance constructor
+ * A ROI instance constructor (Protected constructor. Don't use this constructor)
+ * directly. Use Fetch method of Melown.Roi class or constructor of specific
+ * Roi type.
  * @constructor
  */
 Melown.Roi = function(config_, core_) {
@@ -20,6 +22,51 @@ Melown.Roi = function(config_, core_) {
     // inti roi point
     this._init();
 }
+
+Melown.Roi.Fetch = function(config_, core_, clb_) {
+    var done = function(json_) {
+        if (typeof json_ === 'object' && json_ !== null && json_ !== undefined) {
+            if (typeof json_.type === 'string' 
+                && typeof Melown.Roi.Type[json_.type] === 'function') {
+                clb_(null, new Melown.Roi.Type[json_.type](json_, core_));
+            } else {
+                var err = new Error('Downloaded configuration JSON does not contain registered ROI type');
+                console.error(err);
+                clb(err);
+                return;
+            }
+        }
+        var err = new Error('Downloaded configuration is not JSON object');
+        console.error(err);
+        clb(err);
+        return;
+    }
+
+    if (typeof config_ === 'string') {
+        // async load of config at URL and call processConfig again (with object)
+        // TODO - Vadstena.loadJSON should be replaced by Melown.*
+        Vadstena.loadJSON(config_, done, function(error_) {
+            var err = new Error('Unable to download configuration JSON');
+            console.error(err);
+            clb(err);
+            return;
+        });
+    } else if (typeof config_ !== 'object' 
+        || config_ === null 
+        || config_ === undefined) {
+        var err = new Error('Unknown configuration format passed to Pano browser')
+        console.error(err);
+        clb(err);
+        return;
+    }
+
+    done(config_);
+}
+
+/**
+ * To be fullfilled by specific roi types [type : class]
+ */
+Melown.Roi.Type = {}
 
 /**
  * Roi object states.
@@ -97,12 +144,52 @@ Melown.Roi.orientation = function(yaw, pitch) {
 // Protected methods
 
 Melown.Roi.prototype._init = function() {
-    this._processConfig(this.config_);
+    // Used mostly for subclassing
+    this._loadConfig();
 }
+
+Melown.Roi.prototype._finalizeInit = function() {
+
+}
+
+Melown.Roi._deinit = function() {
+    // Used mostly for subclassing
+}
+
+// Private methods
+
+Melown.Roi.prototype._loadConfig = function() {
+    if (typeof config_ === 'string') {
+        // async load of config at URL and call processConfig again (with object)
+        // TODO - Vadstena.loadJSON should be replaced by Melown.*
+        Vadstena.loadJSON(config_, function(json_) {
+            this.config_ = json_;
+            this._processConfig();
+        }, function(error_) {
+            this.state_ = Melown.Roi.State.Error;
+            console.error('Unable to download configuration JSON');
+        });
+    } else if (typeof config_ !== 'object' 
+        || config_ === null 
+        || config_ === undefined) {
+        this.state_ = Melown.Roi.State.Error;
+        console.error('Unknown configuration format passed to Pano browser');
+    }
+
+    // it's object - just process it
+    this._processConfig();
+}
+
+Melown.Roi.prototype._processConfig = function() {
+    if (this.config_.)
+}
+
+
 
 Melown.Roi.prototype._processConfig = function(config_) {
     if (typeof config_ === 'string') {
         // async load of config at URL and call processConfig again (with object)
+        // TODO - Vadstena.loadJSON should be replaced by Melown.*
         Vadstena.loadJSON(config_, function(json_) {
             this._processConfig(json_);
         }, function(error_) {
