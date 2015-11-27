bd778b01e67d31b9748f14a035135d63cc6a4fab
Tomáš Kavan <tomas.kavan@citationtech.net>
2015-11-27 15:28:51 +0100

    Loaction base structure

---
+++ melown/rois/location/location.js
@@ -0,0 +1,63 @@
+/**
+ * Photo class - specific Roi type class for rendering simple photo.
+ * @constructor
+ * @final
+ * @extends {Melown.Roi}
+ */
+Melown.Roi.Location = function(config_, core_, options_) {
+}
+
+// Inheritance from Roi
+Melown.Roi.Location.prototype = Object.create(Melown.Roi.prototype);
+Melown.Roi.Location.prototype.constructor = Melown.Roi.Location;
+
+// Register class to Roi type dictionary (used by Roi.Fetch function)
+Melown.Roi.Type['location'] = Melown.Roi.Photo;
+
+// Protected methods
+
+Melown.Roi.Pano.prototype._init = function() {
+    // prepare UI
+
+    this.super_._init.call(this);
+}
+
+Melown.Roi.Pano.prototype._processConfig = function() {
+    this.super_._processConfig.call(this);
+
+    if (this.state_ === Melown.Roi.State.Error) {
+        return;
+    }
+
+    var err = null;
+    if (typeof this.config_['location'] !== 'object' 
+        || this.config_['location'] === null) {
+        err = new Error('Missing (or type error) location key in config JSON');
+    } else if (!this.config_['location']['navControl'] instanceof Array 
+        || this.config_['location']['navExtents'].length !== 4) {
+        err = new Error('Missing (or type error) location.navExtents in config JSON');
+    } else if (typeof this.config_['location']['navControl'] !== 'string') {
+        err = new Error('Missing (or type error) location.navControl in config JSON');
+    }
+
+    if (err) {
+        this.state_ = Melown.Roi.State.Error;
+        console.error(err);
+    } else {
+        this.navControl = this.config_['location']['navControl'];
+        this.navExtents = this.config_['location']['navExtents'];
+    }
+}
+
+// Public accessors
+
+Melown.Roi.prototype.alpha = function(alpha_) {
+    if (typeof alpha_ !== "number") {
+        return this.alpha_;
+    }
+
+    // Location roi is allways transparent
+    alpha_ = 0.0;
+
+    this.super_.alpha.call(this, alpha_);
+}
