9406451a2f1d237e3debc1d1fe940b47b897c84f
Tomáš Kavan <tomas.kavan@citationtech.net>
2015-11-27 23:57:12 +0100

    Photo ROI finalized

--- melown/rois/photo/photo.js
+++ melown/rois/photo/photo.js
@@ -15,6 +15,8 @@ Melown.Roi.Photo = function(config_, core_, options_) {
     // claculated config properties
     this.photoMatrices_ = [];
     this.orientationMatrix_ = null;
+    this.texture_ = null;
+    this.image_ = null;
 
     this.super_ = Melown.Roi.prototype;
     Melown.Roi.call(this, config_, core_, options_);
@@ -63,20 +65,28 @@ Melown.Roi.Photo.prototype._processConfig = function() {
         console.error(err);
     } else {
         this.photoOrientation_ = this.config_['photo']['orientation'];
-        this.imageSize_ = this.config_['photo']['imageSize'][0];
-        this.tileTemplate_ = this.config_['photo']['imageUrl'];
+        this.imageSize_ = this.config_['photo']['imageSize'];
+        this.imageUrl_ = this.config_['photo']['imageUrl'];
     }
 }
 
 Melown.Roi.Photo.prototype._initFinalize = function() {
     this.super_._initFinalize.call(this);
 
+    // load texture
+    this.image_ = Melown.Http.imageFactory(this.imageUrl_, function(data_) {
+        this.texture_ = this.renderer_.createTexture({source : this.image_})
+        this.image_ = null;
+    }.bind(this), function(err_) {
+        this.image_ = null;
+        this.state_ = Melown.Roi.State.Error;
+        console.error(err_);
+    }.bind(this));
+
     // orient photo
-    this.orientationMatrix_ = Melown.mat4.create();
-    var rotateZ = Melown.rotationMatrix(2, Melown.radians(-this.photoOrientation_[2]));
+    this.orientationMatrix_ = Melown.rotationMatrix(2, Melown.radians(-this.photoOrientation_[2]));
     var rotateY = Melown.rotationMatrix(1, Melown.radians(-this.photoOrientation_[1]));
     var rotateX = Melown.rotationMatrix(0, Melown.radians(-this.photoOrientation_[0]));
-    Melown.mat4.multiply(this.orientationMatrix_, rotateZ, this.orientationMatrix_);
     Melown.mat4.multiply(this.orientationMatrix_, rotateY, this.orientationMatrix_);
     Melown.mat4.multiply(this.orientationMatrix_, rotateX, this.orientationMatrix_);
 }
@@ -96,10 +106,53 @@ Melown.Roi.Photo.prototype._update = function() {
 }
 
 Melown.Roi.Photo.prototype._draw = function() {
+    if (this.state_ !== Melown.Roi.State.FadingIn 
+        && this.state_ !== Melown.Roi.State.FadingOut
+        && this.state_ !== Melown.Roi.State.Presenting) {
+        return;
+    }
     
-    if (!this.map_) {
+    if (!this.map_ || !this.texture_) {
         return;
     }
 
-    // TODO
+    //  projection-view matrix from map.getCamera()
+    var cam_ = this.map_.getCameraInfo();
+    var pv_ = cam_['view-projection-matrix'];
+
+    var mvp_ = Melown.mat4.create();
+    Melown.mat4.identity(mvp_);
+
+    var trn_ = Melown.translationMatrix(-0.5, -0.5, 0);
+    Melown.mat4.multiply(trn_, mvp_, mvp_);
+
+    var rot_ = Melown.rotationMatrix(0, Melown.radians(180));
+    Melown.mat4.multiply(rot_, mvp_, mvp_);    
+
+    var w = window;
+    var d = document;
+    var e = d.documentElement;
+    var g = d.getElementsByTagName('body')[0];
+    var x = w.innerWidth || e.clientWidth || g.clientWidth;
+    var y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+
+    var sclFacs = [2.2, 2.2];
+    sclFacs[0] = sclFacs[1] * (y/x);
+    if (this.imageSize_[0] > this.imageSize_[1]) {
+        sclFacs[0] = sclFacs[0]*(this.imageSize_[0]/this.imageSize_[1]);
+    } else {
+        sclFacs[1] = sclFacs[1]*(this.imageSize_[1]/this.imageSize_[0]);
+    }
+    var scl_ = Melown.scaleMatrix(sclFacs[0],sclFacs[1],1);
+    //Melown.mat4.multiply(tile_.mat_, mvp_, mvp_);
+    Melown.mat4.multiply(scl_, mvp_, mvp_);
+
+    // draw tile
+    opts_ = {};
+    opts_["mvp"] = mvp_;
+    opts_["texture"] = this.texture_;
+    opts_["color"] = [255, 255, 255, this.alpha_*255];
+    opts_["blend"] = (this.alpha_ < 1.0);
+    this.renderer_.drawBillboard(opts_);
 }
