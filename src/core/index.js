a69d695fc00b5042782757ab2855e2e2be0a7522
David Levinsky <david.levinsky@citationtech.net>
2017-04-28 10:35:53 +0200

    version update

--- src/core/index.js
+++ src/core/index.js
@@ -4,7 +4,6 @@ import {CoreInterface as CoreInterface_} from './interface';
 import {vec2 as vec2_, vec3 as vec3_, vec4 as vec4_, mat3 as mat3_, mat4 as mat4_} from './utils/matrix';
 import {utils as utils_} from './utils/utils';
 import {math as math_} from './utils/math';
-import {Core as Core_} from './core';
 
 //get rid of compiler mess
 var getCoreVersion = getCoreVersion_, checkSupport = checkSupport_;
@@ -12,7 +11,6 @@ var CoreInterface = CoreInterface_;
 var vec2 = vec2_, vec3 = vec3_, vec4 = vec4_, mat3 = mat3_, mat4 = mat4_;
 var utils = utils_;
 var math = math_;
-var Core = Core_;
 var proj4 = Proj4;
 
 
