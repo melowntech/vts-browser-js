3955af4863402b7737f63b1ef287852429ede766
David Levinsky <david.levinsky@citationtech.net>
2017-07-03 16:32:20 +0200

    working geodata builder

--- src/core/map/geodata-builder.js
+++ src/core/map/geodata-builder.js
@@ -7,12 +7,15 @@ var vec3 = vec3_;
 var math = math_;
 
 
-var MapGeodataBuilder = function() {
+var MapGeodataBuilder = function(map) {
     this.map = map;
     this.groups = [];
     this.currentGroup = null;
     this.bboxMin = [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
     this.bboxMax = [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY];
+
+    this.defaultSrs = this.map.getNavigationSrs();
+    this.physSrs = this.map.getPhysicalSrs();
 };
 
 MapGeodataBuilder.prototype.addGroup = function(id) {
@@ -25,64 +28,96 @@ MapGeodataBuilder.prototype.addGroup = function(id) {
     this.currentGroup = this.groups[this.groups.length - 1];
 };
 
-MapGeodataBuilder.prototype.addPoint = function(point, properties, id) {
-    this.currentGroup.points.push({
-        id : id,
-        points : [point],
-        properties : properties
-    });
-};
-
-MapGeodataBuilder.prototype.addPointArray = function(points, properties, id) {
-    this.currentGroup.points.push({
-        id : id,
-        points : points,
-        properties : properties
-    });
-};
-
-MapGeodataBuilder.prototype.addLineString = function(linePoints, properties, id) {
-    this.currentGroup.lines.push({
-        id : id,
-        lines : [linePoints],
-        properties : properties
-    });
-};
-
-MapGeodataBuilder.prototype.addLineStringArray = function(lines, properties, id) {
-    this.currentGroup.lines.push({
-        id : id,
-        lines : lines,
-        properties : properties
-    });
-};
-
-MapGeodataBuilder.prototype.compileGroup = function(group) {
-    bboxMin = [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
-    bboxMax = [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY];
-
-    var geodataGroup = {};
-    var points = this.points, p;
-    var lines = this.lines, line, i, li, j, lj;
-
-    //get group bbox
-    for (i = 0, li = points.length; i < li; i++) {
-        p = points[i];
-
-        if (p[0] > bbopxMax[0]) { bboxMax[0] = p[0]; }
-        if (p[1] > bboxMax[1]) { bboxMax[1] = p[1]; }
-        if (p[2] > bboxMax[2]) { bboxMax[2] = p[2]; }
-
-        if (p[0] < bboxMin[0]) { bboxMin[0] = p[0]; }
-        if (p[1] < bboxMin[1]) { bboxMin[1] = p[1]; }
-        if (p[2] < bboxMin[2]) { bboxMin[2] = p[2]; }
+MapGeodataBuilder.prototype.addPoint = function(point, properties, id, srs) {
+    if (!this.currentGroup) {
+        this.addGroup('some-group');
     }
 
-    for (i = 0, li = lines.length; i < li; i++) {
-        line = lines[i];
+    this.currentGroup.points.push({
+        id : id,
+        points : [ this.physSrs.convertCoordsFrom(point, srs ? srs : this.defaultSrs) ],
+        properties : properties
+    });
+};
 
-        for (ji = 0, lj = line.length; j < lj; j++) {
-            p = line[j];
+MapGeodataBuilder.prototype.addPointArray = function(points, properties, id, srs) {
+    if (!this.currentGroup) {
+        this.addGroup('some-group');
+    }
+
+    var convertedPoints = new Array(points.length);
+    srs = srs ? srs : this.defaultSrs;
+
+    for (var i = 0, li = convertedPoints.length; i < li; i++) {
+        convertedPoints[i] = this.physSrs.convertCoordsFrom(points[i], srs);
+    }
+
+    this.currentGroup.points.push({
+        id : id,
+        points : convertedPoints,
+        properties : properties
+    });
+};
+
+MapGeodataBuilder.prototype.addLineString = function(linePoints, properties, id, srs) {
+    if (!this.currentGroup) {
+        this.addGroup('some-group');
+    }
+
+    var convertedPoints = new Array(linePoints.length);
+    srs = srs ? srs : this.defaultSrs;
+
+    for (var i = 0, li = convertedPoints.length; i < li; i++) {
+        convertedPoints[i] = this.physSrs.convertCoordsFrom(linePoints[i], srs);
+    }
+
+    this.currentGroup.lines.push({
+        id : id,
+        lines : [convertedPoints],
+        properties : properties
+    });
+};
+
+MapGeodataBuilder.prototype.addLineStringArray = function(lines, properties, id, srs) {
+    if (!this.currentGroup) {
+        this.addGroup('some-group');
+    }
+
+    srs = srs ? srs : this.defaultSrs;
+
+    var convertedLines = new Array(lines.length);
+
+    for (var i = 0, li = lines.length; i < li; i++) {
+        var subline = lines[i];
+        var convertedPoints = new Array(subline.length);
+
+        for (var j = 0, lj = convertedPoints.length; j < lj; j++) {
+            convertedPoints[j] = this.physSrs.convertCoordsFrom(subline[j], srs);
+        }
+
+        convertedLines[i] = convertedPoints;
+    }
+
+    this.currentGroup.lines.push({
+        id : id,
+        lines : convertedLines,
+        properties : properties
+    });
+};
+
+MapGeodataBuilder.prototype.compileGroup = function(group, resolution) {
+    var bboxMin = [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
+    var bboxMax = [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY];
+    var geodataGroup = {};
+    var groupPoints = group.points, points, p, feature, finalFeature;
+    var groupLines = group.lines, lines, line, i, li, j, lj, k, lk;
+
+    //get group bbox
+    for (i = 0, li = groupPoints.length; i < li; i++) {
+        points = groupPoints[i].points;
+
+        for (j = 0, lj = points.length; j < lj; j++) {
+            p = points[j];
 
             if (p[0] > bboxMax[0]) { bboxMax[0] = p[0]; }
             if (p[1] > bboxMax[1]) { bboxMax[1] = p[1]; }
@@ -94,13 +129,28 @@ MapGeodataBuilder.prototype.compileGroup = function(group) {
         }
     }
 
-    //process coords to resolution
-    bboxScaleFactor = [resolution/((bboxMax[0] - bboxMin[0]) + 1),
-                       resolution/((bboxMax[1] - bboxMin[1]) + 1),
-                       resolution/((bboxMax[2] - bboxMin[2]) + 1)];
+    for (i = 0, li = groupLines.length; i < li; i++) {
+        lines = groupLines[i].lines;
+
+        for (j = 0, lj = lines.length; j < lj; j++) {
+            line = lines[j];
+
+            for (k = 0, lk = line.length; k < lk; k++) {
+                p = line[k];
+
+                if (p[0] > bboxMax[0]) { bboxMax[0] = p[0]; }
+                if (p[1] > bboxMax[1]) { bboxMax[1] = p[1]; }
+                if (p[2] > bboxMax[2]) { bboxMax[2] = p[2]; }
+
+                if (p[0] < bboxMin[0]) { bboxMin[0] = p[0]; }
+                if (p[1] < bboxMin[1]) { bboxMin[1] = p[1]; }
+                if (p[2] < bboxMin[2]) { bboxMin[2] = p[2]; }
+            }
+        }
+    }
 
     if (!resolution) {
-        var maxDelta = Math.max((bboxMax[0] - bboxMin[0]) + 1, (bboxMax[1] - bboxMin[1]) + 1, (bboxMax[2] - bboxMin[2]) + 1));
+        var maxDelta = Math.max((bboxMax[0] - bboxMin[0]) + 1, (bboxMax[1] - bboxMin[1]) + 1, (bboxMax[2] - bboxMin[2]) + 1);
 
         //25cm resolution
         resolution = maxDelta / 0.25;
@@ -110,35 +160,92 @@ MapGeodataBuilder.prototype.compileGroup = function(group) {
 
     geodataGroup.resolution = resolution;
 
-    var finalPoints = new Array(points.length);
+    //process coords to resolution
+    var bboxScaleFactor = [resolution/((bboxMax[0] - bboxMin[0]) + 1),
+                           resolution/((bboxMax[1] - bboxMin[1]) + 1),
+                           resolution/((bboxMax[2] - bboxMin[2]) + 1)];
 
-    for (i = 0, li = points.length; i < li; i++) {
-        p = points[i];
+    geodataGroup.points = new Array(groupPoints.length);
 
-        finalPoints[i] = [ Math.round((p[0] - bboxMin[0]) * bboxScaleFactor[0]),
-                           Math.round((p[1] - bboxMin[1]) * bboxScaleFactor[1]),
-                           Math.round((p[2] - bboxMin[2]) * bboxScaleFactor[2]) ];
-    }
+    for (i = 0, li = groupPoints.length; i < li; i++) {
+        feature = groupPoints[i]; 
+        points = feature.points;
 
-    geodataGroup.points = finalPoints;
+        var finalPoints = new Array(points.length);
 
-    var finalLines = new Array(lines.length);
+        for (j = 0, lj = points.length; j < lj; j++) {
+            p = points[j];
 
-    for (i = 0, li = lines.length; i < li; i++) {
-        line = lines[i];
-
-        finalPoints = new Array(line.length);
-
-        for (ji = 0, lj = line.length; j < lj; j++) {
-            p = line[j];
-
-            finalPoints[i] = [ Math.round((p[0] - bboxMin[0]) * bboxScaleFactor[0]),
+            finalPoints[j] = [ Math.round((p[0] - bboxMin[0]) * bboxScaleFactor[0]),
                                Math.round((p[1] - bboxMin[1]) * bboxScaleFactor[1]),
                                Math.round((p[2] - bboxMin[2]) * bboxScaleFactor[2]) ];
         }
+
+        finalFeature = {
+            points : finalPoints
+        };
+
+        if (feature.id) {
+            finalFeature.id = feature.id;
+        }
+
+        if (feature.properties) {
+            finalFeature.properties = feature.properties;
+        }
+
+        geodataGroup.points[i] = finalFeature;
     }
 
-    geodataGroup.lines = finalLines;
+    geodataGroup.lines = new Array(groupLines.length);
+
+    for (i = 0, li = groupLines.length; i < li; i++) {
+        feature = groupLines[i]; 
+        lines = feature.lines;
+
+        var finalLines = new Array(lines.length);
+
+        for (j = 0, lj = lines.length; j < lj; j++) {
+            line = lines[j];
+
+            finalPoints = new Array(line.length);
+
+            for (k = 0, lk = line.length; k < lk; k++) {
+                p = line[k];
+
+                finalPoints[k] = [ Math.round((p[0] - bboxMin[0]) * bboxScaleFactor[0]),
+                                   Math.round((p[1] - bboxMin[1]) * bboxScaleFactor[1]),
+                                   Math.round((p[2] - bboxMin[2]) * bboxScaleFactor[2]) ];
+            }
+
+            finalLines[j] = finalPoints;
+        }
+
+        finalFeature = {
+            lines : finalLines
+        };
+
+        if (feature.id) {
+            finalFeature.id = feature.id;
+        }
+
+        if (feature.properties) {
+            finalFeature.properties = feature.properties;
+        }
+
+        geodataGroup.lines[i] = finalFeature;
+    }
+
+    geodataGroup.bbox = [ bboxMin, bboxMax ];
+
+    if (bboxMax[0] > this.bboxMax[0]) { this.bboxMax[0] = bboxMax[0]; }
+    if (bboxMax[1] > this.bboxMax[1]) { this.bboxMax[1] = bboxMax[1]; }
+    if (bboxMax[2] > this.bboxMax[2]) { this.bboxMax[2] = bboxMax[2]; }
+
+    if (bboxMin[0] < this.bboxMin[0]) { this.bboxMin[0] = bboxMin[0]; }
+    if (bboxMin[1] < this.bboxMin[1]) { this.bboxMin[1] = bboxMin[1]; }
+    if (bboxMin[2] < this.bboxMin[2]) { this.bboxMin[2] = bboxMin[2]; }
+
+    return geodataGroup;
 };
 
 MapGeodataBuilder.prototype.makeGeodata = function(resolution) {
@@ -153,11 +260,26 @@ MapGeodataBuilder.prototype.makeGeodata = function(resolution) {
     for (var i = 0, li = this.groups.length; i < li; i++) {
         geodata["groups"].push(this.compileGroup(this.groups[i], resolution));
     }
+
+    return geodata;
 };
 
-MapGeodataBuilder.prototype.makeFreeLayer = function(id, style, resolution) {
+MapGeodataBuilder.prototype.makeFreeLayer = function(style, resolution) {
     var geodata = this.makeGeodata(resolution);
 
+    var freeLayer = {
+            'credits' : [],
+            'displaySize' : 1024,
+            'extents' : {
+                'll' : this.bboxMin,
+                'ur' : this.bboxMax
+            },
+            'geodata' : geodata,
+            'style' : style,
+            'type' : 'geodata'
+        };
+
+    return freeLayer;        
 };
 
 export default MapGeodataBuilder;
