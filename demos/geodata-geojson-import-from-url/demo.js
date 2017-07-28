24ad7236251951306226b1c61ea143be4be56ebd
David Levinsky <david.levinsky@citationtech.net>
2017-07-28 09:10:54 +0200

    new demo

--- demos/geodata-geojson-import-from-url/demo.js
+++ demos/geodata-geojson-import-from-url/demo.js
@@ -13,7 +13,7 @@ var geodata = null;
     // https://github.com/Melown/vts-browser-js/wiki/VTS-Browser-Map-API#definition-of-view
     browser = vts.browser('map-div', {
         map: 'https://cdn.melown.com/mario/store/melown2015/map-config/melown/VTS-Tutorial-map/mapConfig.json',
-        position : [ 'obj', -122.48443455025, 37.83071587047, 'float', 0.00, 19.04, -49.56, 0.00, 1946.45, 55.00 ]
+        position : [ 'obj', 8.151584320, 61.48064554, 'float', 0.00, -23.00, -49.10, 0.00, 41033.50, 55.00 ]
     });
 
     //check whether browser is supported
@@ -29,67 +29,24 @@ var geodata = null;
 
 function onMapLoaded() {
     map = browser.map;
+    vts.utils.loadJSON('./jotunheimen_track.json', geojsonLoaded);
+}
 
+
+function geojsonLoaded(data) {
     //create geodata object
     geodata = map.createGeodata();
 
     //import GeoJSON data
     //polygon are not supported yet
-    geodata.importGeoJson(
-
-        {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-122.48347, 37.82955],
-                    },
-                    "properties": {
-                        "title": "Golden Gate Bridge Vista Point",
-                    }
-                },
-
-                {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                        "type": "LineString",
-                        "coordinates": [
-                            [-122.48369693756, 37.83381888486],
-                            [-122.48344236083, 37.83317489144],
-                            [-122.48335253015, 37.83270036637],
-                            [-122.48361819152, 37.83205636317],
-                            [-122.48404026031, 37.83114119107],
-                            [-122.48404026031, 37.83049717427],
-                            [-122.48348236083, 37.82992094395],
-                            [-122.48356819152, 37.82954808664],
-                            [-122.48507022857, 37.82944639795],
-                            [-122.48610019683, 37.82880236636],
-                            [-122.48695850372, 37.82931081282],
-                            [-122.48700141906, 37.83080223556],
-                            [-122.48751640319, 37.83168351665],
-                            [-122.48803138732, 37.83215804826],
-                            [-122.48888969421, 37.83297152392],
-                            [-122.48987674713, 37.83263257682],
-                            [-122.49043464660, 37.83293762928],
-                            [-122.49125003814, 37.83242920781],
-                            [-122.49163627624, 37.83256478721],
-                            [-122.49223709106, 37.83337825839],
-                            [-122.49378204345, 37.83368330777]
-                        ]
-                    }
-                }
-            ]
-        }
-    );
+    geodata.importGeoJson(data);
 
     //this function is needed only when 'float' heights are used
     //in case you use data with 'fix' height only then you can
     //skip this function and call makeFreeLayer directly
     geodata.processHeights('node-by-precision', 62, onHeightProcessed);
 }
+
 
 function onHeightProcessed() {
     var style = {
