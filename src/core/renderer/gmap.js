e48a95e0aad4126e24517e0ce96c1ff2eaddf8e7
David Levinsky <david.levinsky@citationtech.net>
2018-07-02 18:08:23 +0200

    new version of scr-count

--- src/core/renderer/gmap.js
+++ src/core/renderer/gmap.js
@@ -2,62 +2,31 @@
 
 function processGMap(gpu, gl, renderer, screenPixelSize) {
 
-    var tileCount = 31;                   //labelGridCells
-    var featuresPerSquareInch = 0.6614;   //labelsPerSquareInch
+    var tileCount = renderer.config.mapFeatureGridCells; //31; //labelGridCells
+    var featuresPerSquareInch = renderer.config.mapFeaturesPerSquareInch; //0.6614; //labelsPerSquareInch
     var ppi = 96 * (window.devicePixelRatio || 1);
     var screenLX = renderer.curSize[0];
     var screenLY = renderer.curSize[1];
     var featureCount = (screenLX/ppi)*(screenLY/ppi)*featuresPerSquareInch; 
-    var i, li;
-
+    var i, li, top = false;
 
     //get top features
     var featureCache = renderer.gmap;
     var featureCacheSize = renderer.gmapIndex;
     var topFeatures = renderer.gmapTop;
-    var topFeaturesIndex = 0;
-    var topFeaturesIndex2 = 0;
     var featureCount2 = featureCount;
 
     if (featureCount > featureCacheSize) {
         featureCount2 = featureCacheSize;
     }
 
-    var top = false, value, feature;
-    var currentIndex = 0;
-    var currentValue2 = top ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
-
-    do {
-        var currentValue = top ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
-        topFeaturesIndex2 = topFeaturesIndex;
-
-        for (i = 0, li = featureCacheSize; i < li; i++) {
-            feature = featureCache[i];
-            value = feature[0].reduce[1];
-
-            if (((top && value >= currentValue && value < currentValue2) || (value <= currentValue && value > currentValue2)) ) {
-                if (currentValue != value) {
-                    topFeaturesIndex = topFeaturesIndex2;
-                }
-
-                topFeatures[topFeaturesIndex] = feature;
-                topFeaturesIndex++;
-                currentValue = value;
-            }
-        }
-
-        currentValue2 = currentValue;
-        currentIndex++;
-
-    } while(currentIndex < featureCount2);
-
-
     //distribute top features
     var tileSize = Math.floor(Math.sqrt((screenLX*screenLY) / tileCount));
     var hitMap = renderer.gmapHit, usedFeatures = 0;
+    var tileFeatures, count, feature;
 
     do {
-        var a,b,c,d,ix,iy,is,pp,tx,ty,mx,my,v,index,o;
+        var a,b,c,d,ix,iy,is,pp,tx,ty,mx,my,v,index,o,j;
 
         ix = screenLX / tileSize;
         iy = screenLY / tileSize;
@@ -77,18 +46,26 @@ function processGMap(gpu, gl, renderer, screenPixelSize) {
         c = Math.floor(c * featureCount);
         d = Math.floor(d * featureCount);
 
+        var hitMap = renderer.gmapStore;
+        var hitMapCount = renderer.gmapHit;
+
         //clear hit-map
         for (i = 0, li = (mx+1) * (my+1); i < li; i++) {
-            hitMap[i] = 0;
+            hitMap[i] = null;
         }
 
-        for (i = 0, li = featureCount2; i < li; i++) {
-            feature = topFeatures[i];
+        for (i = 0, li = featureCacheSize; i < li; i++) {
+            feature = featureCache[i];
             if (!feature) {
                 continue;
             }
 
             pp = feature[5];
+
+            if (pp[0] < 0 || pp[0] >= screenLX || pp[1] < 0 || pp[1] >= screenLY) {
+                featureCache[i] = null;
+                continue;
+            }
 
             tx = pp[0] / tileSize;
             ty = pp[1] / tileSize;
@@ -108,11 +85,38 @@ function processGMap(gpu, gl, renderer, screenPixelSize) {
             if (v > 0) {
                 index = Math.floor(tx) + Math.floor(ty) * (mx + 1);
 
-                if (hitMap[index] < v) {
-                    hitMap[index]++;
+                tileFeatures = hitMap[index];
+
+                if (tileFeatures) {
+                    hitMap[index].push(i);
+                } else {
+                    hitMap[index] = [i];
+                    hitMapCount[index] = v;
+                }
+            }
+        }
+
+        for (i = 0, li = (mx+1) * (my+1); i < li; i++) {
+            tileFeatures = hitMap[i];
+
+            if (tileFeatures && tileFeatures.length) {
+                count = hitMapCount[i];
+
+                if (count > tileFeatures.length) {
+                    count = tileFeatures.length;
+                }
+
+                sortFeatures(tileFeatures, top, count, renderer);
+
+                for (j = 0; j < count; j++){
+                    index = topFeatures[j]
+                    feature = featureCache[index];
+                    topFeatures[j] = null;
+                    featureCache[index] = null;
 
                     //render job
                     if (feature[6]) { //no-overlap 
+                        pp = feature[5];
                         o = feature[8];
                         if (!renderer.rmap.addRectangle(pp[0]+o[0], pp[1]+o[1], pp[0]+o[2], pp[1]+o[3], feature[7], feature[0].lastSubJob)) {
                             renderer.rmap.storeRemovedRectangle(pp[0]+o[0], pp[1]+o[1], pp[0]+o[2], pp[1]+o[3], feature[7], feature[0].lastSubJob);
@@ -124,10 +128,8 @@ function processGMap(gpu, gl, renderer, screenPixelSize) {
                             draw.drawGpuSubJob(gpu, gl, renderer, screenPixelSize, subjob, null);
                         }
                     }
-
-                    //remove feature from cache
-                    topFeatures[i] = null;
                 }
+
             }
         }
 
@@ -142,6 +144,45 @@ function processGMap(gpu, gl, renderer, screenPixelSize) {
 
 }
 
+
+function sortFeatures(features, top, count, renderer) {
+    var value, feature;
+    var currentIndex = 0;
+    var currentValue2 = top ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
+    var topFeatures = renderer.gmapTop;
+    var topFeaturesIndex = 0;
+    var topFeaturesIndex2 = 0;
+
+    //remove feature from cache
+    var featureCache = renderer.gmap, index;
+
+
+    do {
+        var currentValue = top ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
+        topFeaturesIndex2 = topFeaturesIndex;
+
+        for (var i = 0, li = features.length; i < li; i++) {
+            index = features[i];
+            feature = featureCache[index];
+            value = feature[0].reduce[1];
+
+            if (((top && value >= currentValue && value < currentValue2) || (value <= currentValue && value > currentValue2)) ) {
+                if (currentValue != value) {
+                    topFeaturesIndex = topFeaturesIndex2;
+                }
+
+                topFeatures[topFeaturesIndex] = index;
+                topFeaturesIndex++;
+                currentValue = value;
+            }
+        }
+
+        currentValue2 = currentValue;
+        currentIndex++;
+
+    } while(currentIndex < count);
+
+}
 
 export {processGMap};
 
